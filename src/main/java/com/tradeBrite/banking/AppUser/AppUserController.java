012538bc1a914164a30cd7ecae2935bfac65042c
Daniel Tokai <tokaidaniel@gmail.com>
2021-10-15 09:49:25 +0200

    removed redundant code snippet for cors policy

--- src/main/java/com/tradeBrite/banking/AppUser/AppUserController.java
+++ src/main/java/com/tradeBrite/banking/AppUser/AppUserController.java
@@ -18,42 +18,36 @@ public class AppUserController {
 
     @PostMapping("add-user/{name}")
     public void addUser(@PathVariable(value = "name") String name, HttpServletResponse response){
-        response.addHeader("Access-Control-Allow-Origin", "*");
         response.setStatus(200);
         appUserService.createNewUser(name);
     }
 
     @GetMapping("get-all-users")
     public List<AppUser> getAllUsers(HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
         response.setStatus(200);
         return appUserService.getAllUsers();
     }
 
     @PostMapping("deposit/{id}/{deposit}")
     public void depositMoney(@PathVariable(value = "id") Long id, @PathVariable(value = "deposit") BigDecimal deposit,HttpServletResponse response){
-        response.addHeader("Access-Control-Allow-Origin", "*");
         response.setStatus(200);
         appUserService.depositMoney(id, deposit);
     }
 
     @PostMapping("withdraw/{id}/{withdraw}")
     public void withdrawMoney(@PathVariable(value = "id") Long id, @PathVariable(value = "withdraw") BigDecimal withdraw,HttpServletResponse response){
-        response.addHeader("Access-Control-Allow-Origin", "*");
         response.setStatus(200);
         appUserService.withdrawMoney(id, withdraw);
     }
 
     @PostMapping("transfer/{sender-id}/{receiver-id}/{transfer}")
     public void transferMoney(@PathVariable(value = "sender-id") Long senderId, @PathVariable(value = "receiver-id") Long receiverId ,@PathVariable(value = "transfer") BigDecimal transfer, HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
         response.setStatus(200);
         appUserService.transferMoney(senderId, receiverId, transfer);
     }
 
     @GetMapping("check-balance/{id}")
     public BigDecimal checkBalance(@PathVariable(value = "id") Long id, HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
         response.setStatus(200);
         return appUserService.checkBalance(id);
     }
